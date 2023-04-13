97b7abc4978daaebb0c4e8cfb3e02d9a5ea2d37b
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-04-13 08:36:07 +0500

    complete queue

--- src/queue.js
+++ src/queue.js
@@ -14,23 +14,35 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
   getUnderlyingList() {
-    return this;
+    return this.head;
   }
 
   enqueue(value) {
-    let current = this;
-    while (current.next) {
-      current = current.next;
+    if (this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = new ListNode(value);
     }
 
-    current.next = new ListNode(value);
+    this.length++;
   }
 
   dequeue() {
-    let current = this;
-    this = current.next;
-    return this;
+    let current = this.head;
+    this.head = current.next;
+    return current.value;
   }
 }
 
